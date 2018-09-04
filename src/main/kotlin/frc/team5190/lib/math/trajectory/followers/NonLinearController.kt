70d5bed32fd68acb5ea879bef9ac273160dfeaec
Prateek Machiraju <prateek.machiraju@gmail.com>
2018-09-03 20:41:40 -0400

    Moved Robot Specific Constants to Constructor

--- src/main/kotlin/frc/team5190/lib/math/trajectory/followers/NonLinearController.kt
+++ src/main/kotlin/frc/team5190/lib/math/trajectory/followers/NonLinearController.kt
@@ -5,7 +5,6 @@
 
 package frc.team5190.lib.math.trajectory.followers
 
-import frc.team5190.lib.extensions.cos
 import frc.team5190.lib.extensions.epsilonEquals
 import frc.team5190.lib.math.geometry.Pose2d
 import frc.team5190.lib.math.geometry.Pose2dWithCurvature
@@ -14,14 +13,15 @@ import frc.team5190.lib.math.trajectory.Trajectory
 import frc.team5190.lib.math.trajectory.TrajectoryIterator
 import frc.team5190.lib.math.trajectory.timing.TimedState
 import frc.team5190.lib.math.trajectory.view.TimedView
-import kotlin.math.PI
 import kotlin.math.sin
 import kotlin.math.sqrt
 
 // https://www.dis.uniroma1.it/~labrob/pub/papers/Ramsete01.pdf
 // Equation 5.12
 
-class NonLinearController(trajectory: Trajectory<TimedState<Pose2dWithCurvature>>) : TrajectoryFollower {
+class NonLinearController(trajectory: Trajectory<TimedState<Pose2dWithCurvature>>,
+                          private val kBeta: Double,
+                          private val kZeta: Double) : TrajectoryFollower {
 
     private val iterator = TrajectoryIterator(TimedView(trajectory))
 
@@ -46,44 +46,23 @@ class NonLinearController(trajectory: Trajectory<TimedState<Pose2dWithCurvature>
 
         return calculateTwist(
                 error = pose inFrameOfReferenceOf robot,
-                pathV = this.point.state.velocity,
-                pathW = this.point.state.velocity * this.point.state.state.curvature
+                vd = this.point.state.velocity,
+                wd = this.point.state.velocity * this.point.state.state.curvature
         ).also { this.point = iterator.advance(dt) }
 
     }
 
-    companion object {
-        private const val kB = 0.30
-        private const val kZeta = 0.85
+    private fun calculateTwist(error: Pose2d,
+                               vd: Double,
+                               wd: Double): Twist2d = Twist2d(
+            vd * error.rotation.cos + gainFunc(vd, wd) * error.translation.x, 0.0,
+            wd + kBeta * sinc(error.rotation.radians) * error.translation.y + gainFunc(vd, wd) * error.rotation.radians
+    )
 
-        private const val kMaxSafeV = 12.0
-        private const val kMaxSafeW = PI
+    private fun gainFunc(v: Double, w: Double) = 2 * kZeta * sqrt((w * w) + ((kBeta) * (v * v)))
 
-        fun calculateTwist(error: Pose2d,
-                           pathV: Double,
-                           pathW: Double) = Twist2d(
-                dx = calculateLinearVelocity(error, pathV, pathW).coerceIn(-kMaxSafeV, kMaxSafeV),
-                dy = 0.0,
-                dtheta = calculateAngularVelocity(error, pathV, pathW).coerceIn(-kMaxSafeW, kMaxSafeW))
-
-
-        private fun calculateLinearVelocity(error: Pose2d, pathV: Double, pathW: Double): Double {
-            return (pathV cos error.rotation.radians) +
-                    gainFunc(pathV, pathW) * error.translation.x
-        }
-
-
-        private fun calculateAngularVelocity(error: Pose2d, pathV: Double, pathW: Double): Double {
-            return pathW +
-                    kB * pathV * sinc(error.rotation.radians) * error.translation.y +
-                    gainFunc(pathV, pathW) * error.rotation.radians
-        }
-
-        private fun gainFunc(v: Double, w: Double) = 2 * kZeta * sqrt((w * w) + ((kB) * (v * v)))
-
-        private fun sinc(theta: Double): Double {
-            return if (theta epsilonEquals 0.0) 1.0 - 1.0 / 6.0 * theta * theta
-            else sin(theta) / theta
-        }
+    private fun sinc(theta: Double): Double {
+        return if (theta epsilonEquals 0.0) 1.0 - 1.0 / 6.0 * theta * theta
+        else sin(theta) / theta
     }
 }
