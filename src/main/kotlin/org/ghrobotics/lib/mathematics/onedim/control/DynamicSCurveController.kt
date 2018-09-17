7b85d53292178ac679b0be3b63cacaaa6992a8af
Prateek Machiraju <prateek.machiraju@gmail.com>
2018-09-17 11:10:57 -0400

    Skeleton for Dynamic S Curve Controller

--- src/main/kotlin/org/ghrobotics/lib/mathematics/onedim/control/DynamicSCurveController.kt
+++ src/main/kotlin/org/ghrobotics/lib/mathematics/onedim/control/DynamicSCurveController.kt
@@ -1,11 +1,41 @@
 package org.ghrobotics.lib.mathematics.onedim.control
 
+import koma.max
+
 class DynamicSCurveController(val x0: Double, val distance: Double,
-                              val maxVelocity: Double, val maxAcceleration: Double, maxJerk: Double): DynamicKinematicsController {
+                              val maxVelocity: Double, val maxAcceleration: Double, val maxJerk: Double) : DynamicKinematicsController {
+
+    private val xConcave = 0.0
+    private val xLinear = 0.0
+
+    private val xAccel = 2 * xConcave + xLinear
+    private val xCruise = distance - 2 * (xConcave + xConcave + xLinear)
+
+    private var velocityCommand = 0.0
+    private var acceleration = 0.0
+
+    // Loops
+    private var lastCallTime = -1.0
+    private var dt = -1.0
 
     override fun getVelocity(currentPos: Double, nanotime: Long): Double {
+
+        dt = if (lastCallTime < 0) 0.0 else nanotime / 1E9 - lastCallTime
+        lastCallTime = nanotime / 1E9
+
         val displacement = currentPos - x0
-        return 0.0
+        acceleration = when {
+            displacement < xConcave                                 -> (acceleration + maxJerk * dt).coerceAtMost(maxAcceleration)
+            displacement < xConcave + xLinear                       -> maxAcceleration
+            displacement < xAccel                                   -> (acceleration - maxJerk * dt).coerceAtLeast(0.0)
+            displacement < xAccel + xCruise                         -> 0.0
+            displacement < xAccel + xCruise + xConcave              -> (acceleration - maxJerk * dt).coerceAtLeast(-maxAcceleration)
+            displacement < xAccel + xCruise + xConcave + xLinear    -> -maxAcceleration
+            displacement < xAccel + xCruise + xAccel                -> (acceleration + maxJerk * dt).coerceAtMost(0.0)
+            else                                                    -> 0.0
+        }
+        velocityCommand = (velocityCommand + acceleration * dt).coerceIn(0.0, maxVelocity)
+        return velocityCommand
     }
 
 }
