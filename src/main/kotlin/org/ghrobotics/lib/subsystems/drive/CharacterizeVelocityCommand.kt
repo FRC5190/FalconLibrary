7b4c5a8ca7bf22d55955a5bd6f39d09b85a9e60c
Prateek Machiraju <prateek.machiraju@gmail.com>
2018-11-05 19:13:31 -0500

    Add coupled characterization

---
+++ src/main/kotlin/org/ghrobotics/lib/subsystems/drive/CharacterizeVelocityCommand.kt
@@ -0,0 +1,52 @@
+package org.ghrobotics.lib.subsystems.drive
+
+import com.ctre.phoenix.motorcontrol.ControlMode
+import org.apache.commons.math3.stat.regression.SimpleRegression
+import org.ghrobotics.lib.commands.FalconCommand
+import org.ghrobotics.lib.mathematics.kEpsilon
+import org.ghrobotics.lib.mathematics.units.Length
+
+class CharacterizeCoupledVelocityCommand(
+    private val driveSubsystem: TankDriveSubsystem,
+    private val wheelRadius: Length
+) : FalconCommand(driveSubsystem) {
+
+    private var commandedVoltage = 0.0
+    private var vIntercept: Double? = null
+    private val voltageSpeedData = HashMap<Double, Double>()
+
+    private val avgSpd
+        get() = (driveSubsystem.leftMaster.sensorVelocity + driveSubsystem.rightMaster.sensorVelocity) / 2.0
+    private val avgRadPerSec
+        get() = avgSpd.value / wheelRadius.value
+
+    init {
+        executeFrequency = 1 // Hz
+        finishCondition += { commandedVoltage > 12.0 }
+    }
+
+    override suspend fun execute() {
+        commandedVoltage += 0.25
+        driveSubsystem.leftMaster.set(ControlMode.PercentOutput, commandedVoltage / 12.0)
+        driveSubsystem.rightMaster.set(ControlMode.PercentOutput, commandedVoltage / 12.0)
+
+        if (avgRadPerSec > kEpsilon) {
+            val averageVoltage =
+                (driveSubsystem.leftMaster.motorOutputVoltage + driveSubsystem.rightMaster.motorOutputVoltage) / 2.0
+
+            if (vIntercept == null) {
+                vIntercept = averageVoltage
+            }
+
+            voltageSpeedData[averageVoltage] = avgRadPerSec
+        }
+    }
+
+    override suspend fun dispose() {
+        val regression = SimpleRegression()
+        voltageSpeedData.forEach { voltage, speed -> regression.addData(voltage, speed) }
+
+        System.out.printf("kV: %3.3f Volts per radians per second, kS: %3.3f Volts. Linearity: %3.3f %n",
+            1 / regression.slope, vIntercept, regression.rSquare)
+    }
+}
