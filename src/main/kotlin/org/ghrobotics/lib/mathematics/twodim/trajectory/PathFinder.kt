1779271006d12440613cb7df1b8fec8f85a989f8
ThePrimedTNT <13050203+ThePrimedTNT@users.noreply.github.com>
2018-11-09 18:14:46 -0500

    improve path finder with optimizations

---
+++ src/main/kotlin/org/ghrobotics/lib/mathematics/twodim/trajectory/PathFinder.kt
@@ -0,0 +1,216 @@
+package org.ghrobotics.lib.mathematics.twodim.trajectory
+
+/* ktlint-disable no-wildcard-imports */
+import org.apache.commons.math3.analysis.interpolation.SplineInterpolator
+import org.apache.commons.math3.geometry.euclidean.twod.Line
+import org.apache.commons.math3.geometry.euclidean.twod.Vector2D
+import org.ghrobotics.lib.mathematics.kEpsilon
+import org.ghrobotics.lib.mathematics.twodim.geometry.Pose2d
+import org.ghrobotics.lib.mathematics.twodim.geometry.Rectangle2d
+import org.ghrobotics.lib.mathematics.twodim.geometry.Translation2d
+import org.ghrobotics.lib.mathematics.units.Length
+import org.ghrobotics.lib.mathematics.units.feet
+import org.ghrobotics.lib.mathematics.units.inch
+import org.ghrobotics.lib.mathematics.units.radian
+import org.ghrobotics.lib.utils.*
+import java.lang.Math.sqrt
+import kotlin.math.pow
+
+class PathFinder(
+    private val robotSize: Length,
+    vararg restrictedAreas: Rectangle2d
+) {
+
+    private val robotSizeCorner = sqrt(robotSize.value.pow(2.0) / 2.0)
+    private val robotCornerTopLeft = Vector2D(-robotSizeCorner, robotSizeCorner)
+    private val robotCornerTopRight = Vector2D(robotSizeCorner, robotSizeCorner)
+    private val robotCornerBottomLeft = Vector2D(-robotSizeCorner, -robotSizeCorner)
+    private val robotCornerBottomRight = Vector2D(robotSizeCorner, -robotSizeCorner)
+    private val robotTopLeftOffset = Translation2d(-robotSize / 2.0, robotSize / 2.0)
+    private val robotBottomRightOffset = Translation2d(robotSize / 2.0, -robotSize / 2.0)
+    private val fieldRectangleWithOffset = Rectangle2d(
+        kFieldRectangle.topLeft - robotTopLeftOffset,
+        kFieldRectangle.bottomRight - robotBottomRightOffset
+    )
+    private val restrictedAreas = restrictedAreas.toList()
+
+    fun findPath(
+        start: Pose2d,
+        end: Pose2d,
+        vararg restrictedAreas: Rectangle2d
+    ): List<Pose2d>? {
+        val pathNodes = findPath(
+            start.translation.toVector2d(),
+            end.translation.toVector2d(),
+            *restrictedAreas
+        ) ?: return null
+
+        val interpolator = SplineInterpolator()
+
+        val indexArray = pathNodes.indices.map { it.toDouble() }.toDoubleArray()
+
+        val funcX = interpolator.interpolate(indexArray, pathNodes.map { it.x }.toDoubleArray())
+            .derivative()
+        val funcY = interpolator.interpolate(indexArray, pathNodes.map { it.y }.toDoubleArray())
+            .derivative()
+
+        return pathNodes.mapIndexed { index, point ->
+            when (index) {
+                0 -> start
+                pathNodes.size - 1 -> end
+                else -> {
+                    val dx = funcX.value(index.toDouble())
+                    val dy = funcY.value(index.toDouble())
+
+                    val angle = Math.atan2(dy, dx).radian
+
+                    Pose2d(point.toTranslation2d(), angle)
+                }
+            }
+        }
+    }
+
+    fun findPath(
+        start: Vector2D,
+        end: Vector2D,
+        vararg restrictedAreas: Rectangle2d
+    ): List<Vector2D>? {
+        val effectiveRestrictedAreas = this.restrictedAreas.plusToSet(restrictedAreas)
+        val worldNodes = createNodes(effectiveRestrictedAreas) + setOf(start, end)
+        return optimize(
+            start,
+            end,
+            worldNodes,
+            effectiveRestrictedAreas
+        )
+    }
+
+    // https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm
+    private fun optimize(
+        source: Vector2D,
+        target: Vector2D,
+        points: Set<Vector2D>,
+        effectiveRestrictedAreas: Set<Rectangle2d>
+    ): List<Vector2D>? {
+        val Q = points.map {
+            Node(it, Double.POSITIVE_INFINITY, null)
+        }.toMutableSet()
+
+        Q.first { it.point == source }.dist = 0.0
+
+        while (Q.isNotEmpty()) {
+            val u = Q.minBy { it.dist }!!
+            Q -= u
+            if (u.point == target) {
+                val S = mutableListOf<Vector2D>()
+                var c: Node? = u
+                while (c != null) {
+                    S.add(0, c.point)
+                    c = c.prev
+                }
+                return S
+            }
+
+            val robotRectangle = u.point.toRobotRectangle()
+            for (v in Q) {
+                val toTranslation = (v.point.subtract(u.point)).toTranslation2d()
+                if (effectiveRestrictedAreas.none { it.doesCollide(robotRectangle, toTranslation) }) {
+                    val alt = u.dist + u.point.distance(v.point)
+                    if (alt < v.dist) {
+                        v.dist = alt
+                        v.prev = u
+                    }
+                }
+            }
+        }
+
+        return null
+    }
+
+    private class Node(
+        val point: Vector2D,
+        var dist: Double,
+        var prev: Node?
+    )
+
+    private fun Vector2D.toRobotRectangle() = Rectangle2d(
+        x - robotSize.value / 3, y - robotSize.value / 3,
+        robotSize.value / 3 * 2, robotSize.value / 3 * 2
+    )
+
+    private fun createNodes(restrictedAreas: Set<Rectangle2d>): Set<Vector2D> {
+        val effectiveRestrictedAreasWithOffsets = restrictedAreas.mapToSet {
+            Rectangle2d(it.topLeft + robotTopLeftOffset, it.bottomRight + robotBottomRightOffset)
+        }
+        val result = createLines(effectiveRestrictedAreasWithOffsets)
+            .combinationPairs()
+            .mapNotNullToSet { it.first.intersection(it.second) }
+        val restrictedCorners = restrictedAreas.flatMapToSet(4) { rect ->
+            arrayOf(
+                rect.topLeft.toVector2d().add(robotCornerTopLeft),
+                rect.topRight.toVector2d().add(robotCornerTopRight),
+                rect.bottomLeft.toVector2d().add(robotCornerBottomLeft),
+                rect.bottomRight.toVector2d().add(robotCornerBottomRight)
+            ).asList()
+        }
+        return result.plusToSet(restrictedCorners)
+            .filterNotToSet { point ->
+                val translation = point.toTranslation2d()
+                !fieldRectangleWithOffset.contains(translation) ||
+                    restrictedAreas.any { it.contains(translation) }
+            }
+    }
+
+    private fun createLines(restrictedAreas: Set<Rectangle2d>): Set<Line> {
+        val restrictedWallLines = (restrictedAreas + fieldRectangleWithOffset)
+            .flatMapToSet(4) { rect ->
+                val topLeft = rect.topLeft.toVector2d()
+                val topRight = rect.topRight.toVector2d()
+                val bottomLeft = rect.bottomLeft.toVector2d()
+                val bottomRight = rect.bottomRight.toVector2d()
+                arrayOf(
+                    Pair(topLeft, topRight),
+                    Pair(topLeft, bottomLeft),
+                    Pair(bottomRight, bottomLeft),
+                    Pair(bottomRight, topRight)
+                ).asList()
+            }.mapToSet { pair ->
+                Triple(pair.first, pair.second, Line(pair.first, pair.second, kEpsilon))
+            }
+        val restrictedBetweenLines = restrictedWallLines
+            .combinationPairs()
+            .mapNotNullToSet { (line1, line2) ->
+                if (!line1.third.isParallelTo(line2.third) ||
+                    line1.third.getOffset(line2.third) < robotSize.value / 2.0
+                ) return@mapNotNullToSet null
+                Line(
+                    line1.first.add(line2.first).scalarMultiply(0.5),
+                    line1.second.add(line2.second).scalarMultiply(0.5),
+                    kEpsilon
+                )
+            }
+        return restrictedWallLines.mapToSet { it.third }.plusToSet(restrictedBetweenLines)
+    }
+
+    private fun Translation2d.toVector2d() = Vector2D(xRaw, yRaw)
+    private fun Vector2D.toTranslation2d() = Translation2d(x, y)
+
+    companion object {
+        private val kFieldRectangle = Rectangle2d(
+            Translation2d(),
+            Translation2d((54 / 2).feet, 27.feet)
+        )
+        val k2018LeftSwitch = Rectangle2d(
+            Translation2d(140.inch, 85.25.inch),
+            Translation2d(196.inch, 238.75.inch)
+        )
+        val k2018Platform = Rectangle2d(
+            Translation2d(261.47.inch, 95.25.inch),
+            Translation2d(386.53.inch, 228.75.inch)
+        )
+        val k2018CubesSwitch = Rectangle2d(
+            Translation2d(196.inch, 85.25.inch),
+            Translation2d(211.inch, 238.75.inch)
+        )
+    }
+}
