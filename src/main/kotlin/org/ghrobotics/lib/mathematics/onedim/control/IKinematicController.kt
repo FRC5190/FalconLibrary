3d7314a5548d7afcfe373740d5342147d7644e1a
Prateek Machiraju <prateek.machiraju@gmail.com>
2018-11-11 15:05:20 -0500

    Remove 1d math -- will not be used

--- src/main/kotlin/org/ghrobotics/lib/mathematics/onedim/control/IKinematicController.kt
+++
@@ -1,5 +0,0 @@
-package org.ghrobotics.lib.mathematics.onedim.control
-
-interface IKinematicController {
-    fun getVelocity(nanotime: Long = System.nanoTime()): PVAData
-}
--- src/main/kotlin/org/ghrobotics/lib/mathematics/onedim/control/PVAData.kt
+++
@@ -1,3 +0,0 @@
-package org.ghrobotics.lib.mathematics.onedim.control
-
-data class PVAData(val x: Double, val v: Double, val a: Double)
