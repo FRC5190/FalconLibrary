723850f4298b1078d91f7429cbf8aeaf5ee75e4a
ThePrimedTNT <13050203+ThePrimedTNT@users.noreply.github.com>
2018-12-13 13:50:25 -0500

    Use Actual Heading

--- src/main/kotlin/org/ghrobotics/lib/subsystems/drive/localization/Localization.kt
+++ src/main/kotlin/org/ghrobotics/lib/subsystems/drive/localization/Localization.kt
@@ -2,8 +2,7 @@ package org.ghrobotics.lib.subsystems.drive.localization
 
 import edu.wpi.first.wpilibj.Timer
 import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.ObsoleteCoroutinesApi
-import kotlinx.coroutines.newSingleThreadContext
+import kotlinx.coroutines.asCoroutineDispatcher
 import kotlinx.coroutines.withContext
 import org.ghrobotics.lib.debug.LiveDashboard
 import org.ghrobotics.lib.mathematics.twodim.geometry.Pose2d
@@ -11,13 +10,15 @@ import org.ghrobotics.lib.mathematics.units.Rotation2d
 import org.ghrobotics.lib.mathematics.units.Time
 import org.ghrobotics.lib.utils.Source
 import org.ghrobotics.lib.utils.launchFrequency
+import java.util.concurrent.Executors
 
 abstract class Localization(
     val robotHeading: Source<Rotation2d>
 ) : Source<Pose2d> {
 
-    @ObsoleteCoroutinesApi
-    private val localizationContext = newSingleThreadContext("Localization")
+    private val localizationContext = Executors.newSingleThreadExecutor {
+        Thread(it, "Localization")
+    }.asCoroutineDispatcher()
 
     /**
      * The robot position relative to the field.
@@ -34,8 +35,8 @@ abstract class Localization(
      * Stores the previous state of the robot.
      */
     private var prevHeading = Rotation2d(0.0)
+    private var headingOffset = Rotation2d(0.0)
 
-    @ObsoleteCoroutinesApi
     suspend fun reset(newPosition: Pose2d = Pose2d()) =
         withContext(localizationContext) {
             resetInternal(newPosition)
@@ -43,11 +44,12 @@ abstract class Localization(
 
     protected open fun resetInternal(newPosition: Pose2d) {
         robotPosition = newPosition
-        prevHeading = robotHeading()
+        val newHeading = robotHeading()
+        prevHeading = newHeading
+        headingOffset = -newHeading
         interpolatableLocalizationBuffer.clear()
     }
 
-    @ObsoleteCoroutinesApi
     internal fun start() {
         GlobalScope.launchFrequency(100, localizationContext) {
             val newHeading = robotHeading()
@@ -55,7 +57,11 @@ abstract class Localization(
             val deltaHeading = newHeading - prevHeading
 
             // Add the recorded motion of the robot during this iteration to the global robot pose.
-            robotPosition += update(deltaHeading)
+            val newRobotPosition = robotPosition + update(deltaHeading)
+            robotPosition = Pose2d(
+                newRobotPosition.translation,
+                newHeading + headingOffset
+            )
 
             // Report new position to Live Dashboard
             LiveDashboard.robotHeading = robotPosition.rotation.radian
