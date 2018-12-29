e7ba70b85ccb0597d60fa0e993719067a40cd4d0
ThePrimedTNT <13050203+ThePrimedTNT@users.noreply.github.com>
2018-12-29 14:35:29 -0500

    Remove unnecessary AHRS sensor classes

--- src/main/kotlin/org/ghrobotics/lib/sensors/AHRS.kt
+++ src/main/kotlin/org/ghrobotics/lib/sensors/AHRS.kt
@@ -6,42 +6,5 @@ import org.ghrobotics.lib.mathematics.units.Rotation2d
 import org.ghrobotics.lib.mathematics.units.degree
 import org.ghrobotics.lib.utils.Source
 
-interface AHRSSensor : Source<Rotation2d> {
-    var angleOffset: Rotation2d
-    val correctedAngle: Rotation2d
-    fun reset()
-    override fun invoke() = correctedAngle
-}
-
-abstract class AHRSSensorImpl : AHRSSensor {
-    protected abstract val sensorYaw: Rotation2d
-    override var angleOffset = 0.degree
-    override val correctedAngle: Rotation2d get() = sensorYaw + angleOffset
-}
-
-fun PigeonIMU.toFalconSensor(): AHRSSensor = Pigeon(this)
-fun AHRS.toFalconSensor(): AHRSSensor = NavX(this)
-
-private class Pigeon(val pigeon: PigeonIMU) : AHRSSensorImpl() {
-    init {
-        reset()
-    }
-
-    override val sensorYaw get() = pigeon.fusedHeading.degree
-
-    override fun reset() {
-        pigeon.setYaw(0.0, 10)
-    }
-}
-
-private class NavX(val navX: AHRS) : AHRSSensorImpl() {
-    init {
-        reset()
-    }
-
-    override val sensorYaw get() = (-navX.fusedHeading).degree
-
-    override fun reset() {
-        navX.reset()
-    }
-}
+fun PigeonIMU.asSource(): Source<Rotation2d> = { fusedHeading.degree }
+fun AHRS.asSource(): Source<Rotation2d> = { (-fusedHeading).degree }
