e16feaed515dd9f043e9f09b8680e49105486d63
Redrield <redrield@gmail.com>
2019-01-07 12:40:55 -0500

    Update VelocityLimitRegionConstraint.kt
    Change Translation2d to Pose2dWithCurvature so that it can be used as a constraint during spline generation

--- src/main/kotlin/org/ghrobotics/lib/mathematics/twodim/trajectory/constraints/VelocityLimitRegionConstraint.kt
+++ src/main/kotlin/org/ghrobotics/lib/mathematics/twodim/trajectory/constraints/VelocityLimitRegionConstraint.kt
@@ -14,24 +14,24 @@
 package org.ghrobotics.lib.mathematics.twodim.trajectory.constraints
 
 import org.ghrobotics.lib.mathematics.twodim.geometry.Rectangle2d
-import org.ghrobotics.lib.mathematics.twodim.geometry.Translation2d
+import org.ghrobotics.lib.mathematics.twodim.geometry.Pose2dWithCurvature
 import org.ghrobotics.lib.mathematics.units.derivedunits.LinearVelocity
 
 class VelocityLimitRegionConstraint internal constructor(
     private val region: Rectangle2d,
     private val velocityLimit: Double
-) : TimingConstraint<Translation2d> {
+) : TimingConstraint<Pose2dWithCurvature> {
 
     constructor(
         region: Rectangle2d,
         velocityLimit: LinearVelocity
     ) : this(region, velocityLimit.value)
 
-    override fun getMaxVelocity(state: Translation2d) =
-        if (state in region) velocityLimit else Double.POSITIVE_INFINITY
+    override fun getMaxVelocity(state: Pose2dWithCurvature) =
+        if (state.pose.translation in region) velocityLimit else Double.POSITIVE_INFINITY
 
     override fun getMinMaxAcceleration(
-        state: Translation2d,
+        state: Pose2dWithCurvature,
         velocity: Double
     ) = TimingConstraint.MinMaxAcceleration.kNoLimits
 }
