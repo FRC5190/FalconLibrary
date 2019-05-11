2472d3ec17338b1279f2dbcaa62c90677155d8de
Prateek Machiraju <prateek.machiraju@gmail.com>
2019-05-10 23:00:34 -0400

    Temporary fix for Jitpack TLS error (thanks nico)

--- wpi/build.gradle.kts
+++ wpi/build.gradle.kts
@@ -2,10 +2,15 @@ plugins {
     id("edu.wpi.first.GradleRIO")
 }
 
+repositories {
+    maven { setUrl("https://maven.woke.engineer/") }
+}
+
 dependencies {
     compile(project(":core"))
-    // Gson
     compile("com.github.salomonbrys.kotson", "kotson", "2.5.0")
 
-    wpi.deps.wpilib().forEach { compile(it) }
+    wpi.deps.wpilibJni().forEach { nativeZip(it) }
+    wpi.deps.wpilibDesktopJni().forEach { nativeDesktopZip(it) }
+    wpi.deps.wpilibJars().forEach { compile(it) }
 }
