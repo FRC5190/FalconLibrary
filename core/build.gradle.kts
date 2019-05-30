b110d3b67167e9b7b1634ad027984d857d8b65f3
Prateek Machiraju <prateek.machiraju@gmail.com>
2019-05-30 09:12:30 -0400

    Bump Keigen to 1.4.0 (#26)

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,5 +1,4 @@
 dependencies {
     compile("org.apache.commons", "commons-math3", "3.6.1")
-    compile("com.github.FRCTeam4069:Keigen:1.3.0")
-
+    compile("com.github.FRCTeam4069:Keigen:1.4.0")
 }
