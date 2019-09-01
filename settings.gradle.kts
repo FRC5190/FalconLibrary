0d71bce38f26fd51e49eb800b016a5a34f8bafca
Prateek Machiraju <prateek.machiraju@gmail.com>
2019-09-01 18:22:27 -0400

    Update Copyright

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,10 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * Copyright 2019, Green Hope Falcons
+ */
+
 rootProject.name = "FalconLibrary"
 include("core", "wpi", "vendorCTRE", "vendorREV", "vendorNAVX", "wpilib2020")
