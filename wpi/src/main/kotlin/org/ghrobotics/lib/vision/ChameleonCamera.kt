db593a8182a4e8c910affd33e41ac94878a48b19
Matt <matthew.morley.ca@gmail.com>
2019-12-30 15:52:58 -0800

    Add 3d deserialization and ChameleonTrackedTarget (#92)

--- wpi/src/main/kotlin/org/ghrobotics/lib/vision/ChameleonCamera.kt
+++ wpi/src/main/kotlin/org/ghrobotics/lib/vision/ChameleonCamera.kt
@@ -8,7 +8,10 @@
 
 package org.ghrobotics.lib.vision
 
+import com.github.salomonbrys.kotson.fromJson
+import com.google.gson.Gson
 import edu.wpi.first.networktables.NetworkTable
+import edu.wpi.first.wpilibj.geometry.Pose2d
 import edu.wpi.first.wpilibj.geometry.Rotation2d
 import org.ghrobotics.lib.mathematics.units.SIUnit
 import org.ghrobotics.lib.mathematics.units.Second
@@ -31,18 +34,41 @@ class ChameleonCamera(cameraName: String) {
     private val latencyEntry = table["latency"]
     private val driverModeEntry = table["driver_mode"]
     private val isValidEntry = table["is_valid"]
+    private val areaEntry = table["area"]
+    private val poseListEntry = table["poseList"]
+    private val altTargetEntry = table["aux_targets"]
+
+    private val gson = Gson()
 
     /**
-     * Returns the vertical angle to the target.
+     * Returns the vertical angle to the best target.
      */
     val pitch: Rotation2d
         get() = Rotation2d.fromDegrees(pitchEntry.getDouble(0.0))
 
     /**
-     * Returns the horizontal angle to the target.
+     * Returns the horizontal angle to the best target.
      */
     val yaw: Rotation2d
         get() = Rotation2d.fromDegrees(-yawEntry.getDouble(0.0)) // Negating to make it CCW positive.
+
+    /**
+     * The area of the best target as a percentage of the total area of the screen.
+     */
+    val area: Double
+        get() = areaEntry.getDouble(0.0)
+
+    /**
+     * The poses of all the targets detected.
+     */
+    val targetPoses: List<Pose2d>
+        get() = gson.fromJson(poseListEntry.getString("[]"))
+
+    /**
+     * The camera relative pose of the best target.
+     */
+    val pose: Pose2d?
+        get() = targetPoses.firstOrNull()
 
     /**
      * Represents the latency in the pipeline between the capture
@@ -56,6 +82,18 @@ class ChameleonCamera(cameraName: String) {
      */
     val isValid: Boolean
         get() = isValidEntry.getBoolean(false)
+
+    /**
+     * Returns a list representing all the targets currently being tracked, as ordered by the selected sorting method.
+     */
+    val altTargetList: List<ChameleonTrackedTarget>
+        get() = gson.fromJson(altTargetEntry.getString("[]"))
+
+    /**
+     * Returns the current best ChameleonTrackedTarget.
+     */
+    val bestTarget: ChameleonTrackedTarget?
+        get() = altTargetList.firstOrNull()
 
     /**
      * Gets or sets the value of the pipeline. This can be used
@@ -76,4 +114,11 @@ class ChameleonCamera(cameraName: String) {
         set(value) {
             driverModeEntry.setBoolean(value)
         }
+
+    data class ChameleonTrackedTarget(
+        val pitch: Double,
+        val yaw: Double,
+        val area: Double,
+        val cameraRelativePose: Pose2d
+    )
 }
