0dc06c2944ee717fbe95868691a27d2925d6c03c
Prateek Machiraju <prateek.machiraju@gmail.com>
2020-01-28 15:47:25 -0500

    Add smart current limit wrapper to FalconMAX

--- vendorREV/src/main/kotlin/org/ghrobotics/lib/motors/rev/FalconMAX.kt
+++ vendorREV/src/main/kotlin/org/ghrobotics/lib/motors/rev/FalconMAX.kt
@@ -13,7 +13,6 @@ import com.revrobotics.CANPIDController
 import com.revrobotics.CANSparkMax
 import com.revrobotics.CANSparkMaxLowLevel
 import com.revrobotics.ControlType
-import kotlin.properties.Delegates
 import org.ghrobotics.lib.mathematics.units.Ampere
 import org.ghrobotics.lib.mathematics.units.SIKey
 import org.ghrobotics.lib.mathematics.units.SIUnit
@@ -26,6 +25,7 @@ import org.ghrobotics.lib.mathematics.units.inAmps
 import org.ghrobotics.lib.mathematics.units.nativeunit.NativeUnitModel
 import org.ghrobotics.lib.motors.AbstractFalconMotor
 import org.ghrobotics.lib.motors.FalconMotor
+import kotlin.properties.Delegates
 
 /**
  * Creates a Spark MAX motor controller. The alternate encoder CPR is defaulted
