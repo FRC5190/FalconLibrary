10699dd7fcd11d01472a43658319235aebf76ebd
Prateek Machiraju <prateek.machiraju@gmail.com>
2020-03-28 12:04:21 -0400

    Add classes from 2020 robot code

---
+++ core/src/main/kotlin/org/ghrobotics/lib/utils/MiscellaneousExtensions.kt
@@ -0,0 +1,25 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * Copyright 2019, Green Hope Falcons
+ */
+
+package org.ghrobotics.lib.utils
+
+import edu.wpi.first.wpilibj.geometry.Pose2d
+import edu.wpi.first.wpilibj.geometry.Transform2d
+import org.ghrobotics.lib.motors.rev.FalconMAX
+
+/**
+ * Converts a Pose2d to a Transform2d.
+ */
+fun Pose2d.toTransform(): Transform2d = minus(Pose2d())
+
+/**
+ * Converts a Transform2d to a Pose2d.
+ */
+fun Transform2d.toPose(): Pose2d = Pose2d(translation, rotation)
+
+fun FalconMAX<*>.isConnected() = canSparkMax.firmwareString != "v0.0.0"
