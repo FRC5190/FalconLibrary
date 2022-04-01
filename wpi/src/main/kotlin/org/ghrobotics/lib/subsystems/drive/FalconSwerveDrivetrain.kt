11fa79207c3e2d2c076c269c805ac8b23b30ff8a
Gavin Leach <gcleach05@gmail.com>
2022-04-01 14:40:11 -0700

    Swerve Module Type Fixes

--- wpi/src/main/kotlin/org/ghrobotics/lib/subsystems/drive/FalconSwerveDrivetrain.kt
+++ wpi/src/main/kotlin/org/ghrobotics/lib/subsystems/drive/FalconSwerveDrivetrain.kt
@@ -65,7 +65,7 @@ abstract class FalconSwerveDrivetrain : TrajectoryTrackerSwerveDriveBase(), Sens
     /**
      * The left front motor
      */
-    abstract val modules: Array<AbstractFalconSwerveModule>
+    protected abstract val modules: Array<out AbstractFalconSwerveModule>
 
     abstract val wheelbase: Double
 
